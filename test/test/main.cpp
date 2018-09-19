99c596547fe081a2bebcc657fdde73c68b3fb34d
Aymeric <aymeric@service-WIFI-EDUROAM-LA-18-10.nat.polymtl.ca>
2018-09-19 12:14:31 -0400

    testest

---
+++ test/test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by Aymeric on 2018-09-19.
+//  Copyright © 2018 Aymeric. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
